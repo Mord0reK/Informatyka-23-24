fd189a2650f716cca8fce2cad8611aa10d4e030d
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-02-22 19:00:16 +0100

    Update 1.6.cpp

--- c++/Operatory wyjscia-wejscia 1.1/Zestaw 2 zadania/1.6.cpp
+++ c++/Operatory wyjscia-wejscia 1.1/Zestaw 2 zadania/1.6.cpp
@@ -1,4 +1,10 @@
-﻿#define _USE_MATH_DEFINES
+//Napisz program, który potrafi odszukać miejsce zerowe dowolnej prostej zadanej w postaci
+//kierunkowej y= ax + b (przy założeniu, że a≠ 0). Program :
+//a. Pobiera od użytkownika współczynniki a i b
+//b. W kolejnym wierszu wypisuje znalezione miejsce zerowe
+//c. W kolejnym wierszu wykonuje pauzę systemową
+
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <iomanip>
 #include <math.h>
