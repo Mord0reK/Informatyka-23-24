c64ba52489fbb5265f98b74681f5fa78f6d58b4e
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-02-22 18:59:01 +0100

    Add files via upload

---
+++ c++/Operatory wyjscia-wejscia 1.1/Zestaw 2 zadania/1.6.cpp
@@ -0,0 +1,19 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float a, b, zerowe = 0, pauza;
+    cin >> a >> b;
+    if (a)
+    {
+        zerowe = -b / a;
+        cout << "Znaleziono miejsce zerowe. Wspolrzedne: (" << zerowe << ",0)" << endl;
+        cin >> pauza;
+    }
+}
+
