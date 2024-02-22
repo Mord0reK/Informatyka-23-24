6a80f3c60fee94e1728a0ea0216abe42f0c5314b
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-02-22 18:51:56 +0100

    Update 1.3.cpp

--- c++/Operatory wyjscia-wejscia 1.1/Zestaw 1 zadania/1.3.cpp
+++ c++/Operatory wyjscia-wejscia 1.1/Zestaw 1 zadania/1.3.cpp
@@ -1,6 +1,6 @@
-#Napisz program, który wyświetla na ekranie komputera pierwiastek kwadratowy z wartości predefiniowanej
-#stałej π = 3,14... Należy przyjąć format wyświetlania tego pierwiastka z dokładnością dwóch miejsc po
-#kropce.
+//Napisz program, który wyświetla na ekranie komputera pierwiastek kwadratowy z wartości predefiniowanej
+//stałej π = 3,14... Należy przyjąć format wyświetlania tego pierwiastka z dokładnością dwóch miejsc po
+//kropce.
 
 
 
