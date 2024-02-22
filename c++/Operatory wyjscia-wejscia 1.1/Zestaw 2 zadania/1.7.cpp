8caaef544b4067c464ecfcc8111dc7b1e653a6dc
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-02-22 19:06:38 +0100

    Add files via upload

---
+++ c++/Operatory wyjscia-wejscia 1.1/Zestaw 2 zadania/1.7.cpp
@@ -0,0 +1,23 @@
+﻿//Napisz program, który:
+//a.Pobiera trzy znaki
+//b.W kolejnych wierszach wypisuje wszystkie możliwe permutacje tych znaków.Przykładowo
+//dla znaków ‘K’, ‘O’ i ‘T’ program wypisuje(w dowolnej kolejności) : „KOT”, „KTO”, „OKT”,
+//„OTK”, „TOK” i „TKO”.
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string znaki;
+    cout << "Podaj trzy znaki (bez spacji): ";
+    cin >> znaki;
+
+    sort(znaki.begin(), znaki.end());
+    do {
+        cout << znaki << "\n";
+    } while (next_permutation(znaki.begin(), znaki.end()));
+
+    return 0;
+}
