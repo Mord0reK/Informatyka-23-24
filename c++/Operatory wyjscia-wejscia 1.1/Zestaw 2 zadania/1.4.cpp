54bc9a8a3672a2f99c7341e5dd56d5c270486ab2
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-02-22 18:54:24 +0100

    Update 1.4.cpp

--- c++/Operatory wyjscia-wejscia 1.1/Zestaw 2 zadania/1.4.cpp
+++ c++/Operatory wyjscia-wejscia 1.1/Zestaw 2 zadania/1.4.cpp
@@ -1,4 +1,12 @@
-﻿#define _USE_MATH_DEFINES
+//Napisz program., który:
+//a. Pobiera dwie liczby całkowite u i v
+//b. Oblicza sumę i różnicę tych liczb i zapamiętuje w zmiennych sum i roz
+//c. W kolejnym wierszu wyświetla tekst „u+v = ” oraz sumę
+//d. W kolejnym wierszu wyświetla „u-v = ”oraz różnicę
+//e. W kolejnym wierszu czeka na wprowadzenie dowolnego znaku
+//f. Zwraca do systemu liczbę -1
+
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <iomanip>
 #include <math.h>
