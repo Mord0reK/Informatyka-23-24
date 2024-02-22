918b93d33555945a2e3b101d3dd8eced43aaa0e4
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-02-22 18:48:15 +0100

    Update 1.2.cpp

--- c++/Operatory wyjscia-wejscia 1.1/Zestaw 1 zadania/1.2.cpp
+++ c++/Operatory wyjscia-wejscia 1.1/Zestaw 1 zadania/1.2.cpp
@@ -1,4 +1,8 @@
-﻿#define _USE_MATH_DEFINES
+#Napisz program, który wyświetla na ekranie komputera wartość predefiniowanej stałej π = 3,14… Należy
+#przyjąć format prezentowania tej stałej, oznaczanej w języku C++ jako M_PI, z dokładnością pięciu miejsc po
+#kropce.
+
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <math.h>
 #include <iomanip>
