84dd5e9aa253d7fb5810e128c1d9717bad4ffd88
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-02-22 18:58:33 +0100

    Add files via upload

---
+++ c++/Operatory wyjscia-wejscia 1.1/Zestaw 2 zadania/1.5.cpp
@@ -0,0 +1,17 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float x, y;
+    cout << "x = ";
+    cin >> x;
+    cout << "y = ";
+    cin >> y;
+    cout << "x/y = " << x / y << endl;
+}
+
