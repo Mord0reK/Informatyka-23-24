fb8ce738e8cea8c87016ae7a1b5131fa6bbe0ce8
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-03-02 12:10:48 +0100

    Add files via upload

---
+++ c++/Instrukcje Warunkowe 1.2/Zestaw 1 Zadania/1.1.cpp
@@ -0,0 +1,18 @@
+// Napisz program, który dla trzech boków trójk¹ta a, b i c wprowadzonych z klawiatury sprawdza, czy tworz¹
+// one trójk¹t prostok¹tny (zak³adamy, ¿e a > 0, b > 0, c > 0).
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (pow(a, 2) + pow(b, 2) = pow(c 2))
+    {
+        cout << "Trojkat jest prostokatny" << endl;
+    }
+}
