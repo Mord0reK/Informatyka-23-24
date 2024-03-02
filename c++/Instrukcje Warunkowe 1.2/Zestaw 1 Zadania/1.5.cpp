99f7604a039f6f239b091fc1c3edc45d2ae32117
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-03-02 13:04:16 +0100

    Update 1.5.cpp

--- c++/Instrukcje Warunkowe 1.2/Zestaw 1 Zadania/1.5.cpp
+++ c++/Instrukcje Warunkowe 1.2/Zestaw 1 Zadania/1.5.cpp
@@ -1,38 +1,23 @@
-﻿#define _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
 #include <iostream>
-#include <iomanip>
-#include <math.h>
-
+#include <algorithm>
+#include <ctime>
 using namespace std;
 
 int main()
 {
-    float a, b, c, x1, x2, d, d1;
-    cin >> a >> b >> c;
-    d1 = 4 * a * c;
-    if (a > 0)
+    srand(time(nullptr));
+    int losowana = 0, wpisana = 0;
+    losowana = rand() % 9;
+    cin >> wpisana;
+    cout << losowana;
+    if (wpisana == losowana)
     {
-        d = pow(b, 2) - d1;
-        switch (int(signbit(d)))
-        {
-            case 0:
-            {
-                x1 = (-b - sqrt(d)) / 2;
-                x2 = (-b + sqrt(d)) / 2;
-                cout << x1 << endl << x2 << endl;
-                break;
-            }
-            case 1:
-            {
-                cout << "brak" << endl;
-                break;
-            }
-            default:
-            {
-                x1 = -b / 2 * a;
-                cout << x1 << endl;
-            }
-        }
+        cout << "zgadles" << endl;
+    }
+    else
+    {
+        cout << "pudlo" << endl;
     }
 }
 
