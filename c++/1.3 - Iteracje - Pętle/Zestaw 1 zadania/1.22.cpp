a78b68c65b0deea983bba9254a2e4a8f1df95689
Marcel Stosio <marcel.stosio51@gmail.com>
2024-03-16 13:01:05 +0100

    1.16 do 1.24

---
+++ c++/1.3 - Iteracje - Pętle/Zestaw 1 zadania/1.22.cpp
@@ -0,0 +1,1 @@
+// Napisz program, który wyœwietla du¿e litery alfabetu od A do Z i od Z do A, z// wykorzystaniem pêtli for.#include <iostream>using namespace std;int main() {    for (char x = 65; x <= 90; x++)     {        cout << x << " ";    }    cout << endl;    for (char y = 90; y >= 65; y--)     {        cout << y << " ";    }    return 0;}
