22425cbdf206e400f7722903ec09d4044ededef7
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-03-17 20:07:40 +0100

    Create 1.12.cpp

---
+++ c++/1.3 - Iteracje - Pętle/Zestaw 2 zadania/1.12.cpp
@@ -0,0 +1,21 @@
+// Napisz program, który policzy i wypisze silnię zadanej liczby n, gdzie n > 0.
+// Jeśli użytkownik poda liczbę ujemną, program powinien wypisać „BŁĄD”.Silnia liczb n jest
+// iloczynem kolejnych liczb naturalnych od 1 do n(dla zera wartość silni wynosi 1).
+// Program powinien poprawnie policzyć silnię 20
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double n, silnia = 1, i;
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        silnia *= i;
+    }
+    cout << silnia << endl;
+    return 0;
+}
+
