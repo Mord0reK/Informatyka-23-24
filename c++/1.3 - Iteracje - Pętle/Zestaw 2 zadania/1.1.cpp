aab15e8b6f7dc7de261ab98a62ad933d548a89ca
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-03-17 14:57:41 +0100

    Update Zestaw 2 zadania

---
+++ c++/1.3 - Iteracje - Pętle/Zestaw 2 zadania/1.1.cpp
@@ -0,0 +1,8 @@
+//Napisz program, który:
+//a. Pobiera od użytkownika dwie liczby rzeczywiste
+//b. Program dodaje liczby i zapamiętuje ich sumę w zmiennej SUM
+//c. Jeśli wartość SUM jest większa od 100, to program się kończy
+//d. Program pobiera kolejną liczbę, dodaje ją do SUM i ponownie sprawdza, czy nie jest większa 
+//od 100 (skok do punktu d). 
+//Zastosuj pętlę while.
+
