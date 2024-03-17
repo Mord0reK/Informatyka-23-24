ebc418e337a0522992a972542f1551845f048d19
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-03-17 20:18:25 +0100

    Update 1.13.cpp

--- c++/1.3 - Iteracje - Pętle/Zestaw 2 zadania/1.13.cpp
+++ c++/1.3 - Iteracje - Pętle/Zestaw 2 zadania/1.13.cpp
@@ -1,1 +1,23 @@
+// Napisz program, który:
+// a.pobiera od użytkownika liczbę całkowitą dodatnią n
+// b.pobiera n liczb całkowitych
+// c.wypisuje w kolejnych wierszach sumę oraz iloczyn liczb pobranych w punkcie b
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double n, i, sum = 0, liczba, iloczyn = 1;
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cin >> liczba;
+        sum += liczba;
+        iloczyn *= liczba;
+    }
+    cout << sum << endl;
+    cout << iloczyn << endl;
+    return 0;
+}
