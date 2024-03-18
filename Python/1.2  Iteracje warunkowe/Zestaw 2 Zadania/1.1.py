fb4f0af176668f50ca8d0c8f767e335f85912168
Feejd <71599497+Feejd@users.noreply.github.com>
2024-03-18 23:13:06 +0100

    Create 1.1.py

---
+++ Python/1.2  Iteracje warunkowe/Zestaw 2 Zadania/1.1.py
@@ -0,0 +1,9 @@
+# Pobranie dwóch liczb od użytkownika
+a = int(input("Podaj pierwszą liczbę: "))
+b = int(input("Podaj drugą liczbę: "))
+
+# Porównanie i wyświetlenie większej liczby
+if a > b:
+    print("Większa liczba to:", a)
+else:
+    print("Większa liczba to:", b)
