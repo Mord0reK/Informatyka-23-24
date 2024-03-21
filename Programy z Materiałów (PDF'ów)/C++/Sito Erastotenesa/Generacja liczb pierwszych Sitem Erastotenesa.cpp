491bcecffb14346bd02d146b4be036a282757cc1
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-03-21 15:41:33 +0100

    Update Generacja liczb pierwszych Sitem Erastotenesa.cpp

--- Programy z Materiałów (PDF'ów)/C++/Sito Erastotenesa/Generacja liczb pierwszych Sitem Erastotenesa.cpp
+++ Programy z Materiałów (PDF'ów)/C++/Sito Erastotenesa/Generacja liczb pierwszych Sitem Erastotenesa.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+int main()
+  {
+     unsigned int g,i,n,w;
+     bool * S; // Lista dynamiczna 
+     cin >> n;
+     S = new bool[n + 1]; // Wypełnienie listy danymi Bool (true,false) 
+     for(i = 2; i <= n; i++) S[i] = true; // Ustawianie każdego elementu w liscie na wartość True
+     g = (unsigned int)sqrt(n); // Wyznaczenie granicy szukania liczb pierwszych do pierwiastka z n
+     for(i = 2; i <= g; i++)
+       if(S[i])
+       {
+         w = i * i;
+         while(w <= n)
+         {
+           S[w] = false; w += i;
+         }
+       } 
+     for(i = 2; i <= n; i++) if(S[i]) cout << i << " "; // Wypisywanie liczb pierwszych
+     cout << endl;
+     delete [] S; // Usuwanie listy z pamięci komputera
+     return 0;
+}
