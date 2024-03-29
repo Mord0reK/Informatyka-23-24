77fdac6a9a45c9e96136a6dae4cc3ca1fce7e1d1
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-03-29 11:48:12 +0100

    Create 4.1.5 - Tablica dwuwymiarowa liczby 1 po przekatnej odwrotnie.cpp

---
+++ Zestawy Zadań/C++/4 - Tablice/Zestaw 1 zadania/4.1.5 - Tablica dwuwymiarowa liczby 1 po przekatnej odwrotnie.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int dane[10][10], sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (i + j == 9)
+            {
+                dane[i][j] = 1;
+                sum += dane[i][j];
+            }
+            else
+            {
+                dane[i][j] = 0;
+            }
+            cout << dane[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "Suma: " << sum << endl;
+    return 0;
+}
