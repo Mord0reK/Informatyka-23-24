272c397a164f8c4c640a8d4f1d8149c91c1d6632
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-04-16 17:47:54 +0200

    Update 1.22.cpp

--- Zestawy Zadań/C++/1.3 - Iteracje - Pętle/Zestaw 1 zadania/1.22.cpp
+++ Zestawy Zadań/C++/1.3 - Iteracje - Pętle/Zestaw 1 zadania/1.22.cpp
@@ -1,1 +1,19 @@
-// Napisz program, który wyœwietla du¿e litery alfabetu od A do Z i od Z do A, z// wykorzystaniem pêtli for.#include <iostream>using namespace std;int main() {    for (char x = 65; x <= 90; x++)     {        cout << x << " ";    }    cout << endl;    for (char y = 90; y >= 65; y--)     {        cout << y << " ";    }    return 0;}
+// Napisz program, ktÃ³ry wyÅ“wietla duÂ¿e litery alfabetu od A do Z i od Z do A, z
+// wykorzystaniem pÃªtli for.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (char x = 65; x <= 90; x++) 
+    {
+        cout << x << " ";
+    }
+    cout << endl;
+    for (char y = 90; y >= 65; y--) 
+    {
+        cout << y << " ";
+    }
+    return 0;
+}
