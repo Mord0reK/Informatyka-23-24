cf49c0170472d07cbd1a3db9c7d7f5d118950bc4
Mord0reK <135718526+Mord0reK@users.noreply.github.com>
2024-04-17 16:53:11 +0200

    Create Pierwszość liczby naturalnej - algorytm naiwny cz 1.cpp

---
+++ Programy z Materiałów (PDF'ów)/C++/Sito Erastotenesa/Pierwszość liczby naturalnej - algorytm naiwny cz 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int g, i, p;
+	bool t = true;
+	cin >> p;
+	g = (unsigned long long)sqrt(p);
+	for (i = 2; i <= g; i++)
+	{
+		if (p % i == 0)
+		{
+			cout << "Nie" << endl;
+			return 0;
+		}
+	}
+	cout << "Tak" << endl;
+	cout << endl;
+	return 0;
+}
