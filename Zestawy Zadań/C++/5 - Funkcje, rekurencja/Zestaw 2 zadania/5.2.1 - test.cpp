7fed7545508c1d1d814da35f0a471b42cd13f06b
Mord0reK <marcel.stosio51@gmail.com>
2024-04-30 19:17:35 +0200

    Dodanie Programu

--- Zestawy Zadań/C++/5 - Funkcje, rekurencja/Zestaw 2 zadania/5.2.1 - test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Jebac disa kurwe zwisa" << endl;
-    return 0;
-}
---
+++ Zestawy Zadań/C++/5 - Funkcje, rekurencja/Zestaw 2 zadania/5.2.2 - XD.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void MIX(int a)
+{
+    a = a - 20;
+}
+
+
+int main()
+{
+    int L;
+    cin >> L;
+    MIX(L);
+    return 0;
+}
